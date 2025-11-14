c0731932926b3adf21e1f781ad219d406abfd789
vanessaheh <Vanessa.he4@education.nsw.gov.au>
2025-11-14 12:32:44 +1100

    uqfegiulerg
    bno ideA WHATS HAPPEN

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private SpriteRenderer theSR;
+    public Sprite defaultImage;
+    public Sprite pressedImage;
+
+    public KeyCode keytoPress;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        theSR = GetComponent<SpriteRenderer>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(keytoPress))  
+        {
+            theSR.sprite = pressedImage;
+        }
+        
+        if(Input.GetKeyUp(keytoPress))
+        {
+            theSR.sprite = defaultImage;
+        }
+    }
+}
